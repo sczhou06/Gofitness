7ed00951754011ba5327568abcc98e52419784b2
Peter <mr.clcchen@gmail.com>
2019-11-08 14:44:36 -0800

    Init

---
+++ server/src/main/java/com/flagcamp/gofitness/GoFitnessApplication.java
@@ -0,0 +1,13 @@
+package com.flagcamp.gofitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoFitnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoFitnessApplication.class, args);
+	}
+
+}
