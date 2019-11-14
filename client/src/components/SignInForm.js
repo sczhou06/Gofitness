a20e471bbe2d112ac7d5a0b5d24fbf0cb1b32183
ellajialing <ellajialing@gmail.com>
2019-11-14 14:38:36 -0800

    login trail 1

--- client/src/components/SignInForm.js
+++ client/src/components/SignInForm.js
@@ -10,10 +10,37 @@ class SignInForm extends React.Component {
       email: "",
       password: ""
     };
+    this.emailChange = this.emailChange.bind(this);
+    this.passwordChange = this.passwordChange.bind(this);
+    this.submit = this.submit.bind(this);
+    this.getConnection = this.getConnection.bind(this);
   }
 
-  handleLogin = (e) => {
+  emailChange(e) {
+    this.setState({email : e.target.value})
+  }
 
+  passwordChange(e) {
+    this.setState({password : e.target.value})
+  }
+
+  submit() {
+    this.getConnection();
+  }
+
+  getConnection() {
+    let text = {email: this.state.email, password: this.state.password}
+    let send = JSON.stringify(text);
+    fetch(`http://localhost:8181/trainee/login`, {
+      method: 'POST',
+      headers: {'Content-Type':'application/json; charset=utf-8'},
+      body: send
+    }).then(res => res.json()).then(
+      data => {
+        if (data.status === 'OK') window.alert('Success');
+        else window.alert('failed')
+      }
+    )
   }
 
   setUserInfo(event, key) {
@@ -32,26 +59,20 @@ class SignInForm extends React.Component {
               className="formInput"
               prefix={<Icon type="user" className="signInIcon" />}
               placeholder="Username"
-              type="text" onChange={ (e) => {
-                this.setUserInfo(e, "email")
-            }}
+              type="text" onChange={this.emailChange}
             />
           </Form.Item>
           <Form.Item>
             <Input
               className="formInput"
               prefix={<Icon type="lock" className="signInIcon" />}
-              type="password" onChange={ (e) => {
-                this.setUserInfo(e, "password")
-            }}
+              type="password" onChange={this.passwordChange}
               placeholder="Password"
 
             />
           </Form.Item>
           <Form.Item className="register-link">
-            <Button type="submit" onClick={ () => {
-              alert(this.state.email + ":" + this.state.password);
-            }}
+            <Button type="submit" onClick={this.submit}
                     className="login-form-button">
               Log in
             </Button>
