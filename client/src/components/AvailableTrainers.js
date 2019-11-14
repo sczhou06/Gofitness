e336e3a1e43bdaab46f7af4b85496842bffdfbee
ellajialing <ellajialing@gmail.com>
2019-11-14 12:34:14 -0800

    trainee page v1.3

--- client/src/components/AvailableTrainers.js
+++ client/src/components/AvailableTrainers.js
@@ -1,12 +1,55 @@
 import React from "react";
+import {Col, Row} from "react-bootstrap";
+import {Button} from "antd";
+import trainer1 from "../assets/img/trainer1.jpg";
+import {Link} from "react-router-dom"
 
 class AvailableTrainers extends React.Component {
 
   render() {
     return(
-      <div>
-        Available Trainers
-      </div>
+      <Row>
+        <Col xs={6} sm={4} md={3} className="trainerInfo">
+          <img
+            className="trainerImg"
+            src={trainer1}
+            alt="Trainer 1"
+          />
+          <h4 className="trainerName">Trainer's Name</h4>
+          <p className="trainerState">Trainer's Statement</p>
+          <p><Link to="/chat" className="chatBtn btn-primary btn-learn">Chat</Link></p>
+        </Col>
+        <Col xs={6} sm={4} md={3} className="trainerInfo">
+          <img
+            className="trainerImg"
+            src={trainer1}
+            alt="Trainer 1"
+          />
+          <h4 className="trainerName">Trainer's Name</h4>
+          <p className="trainerState">Trainer's Statement</p>
+          <p><Link to="/chat" className="chatBtn btn-primary btn-learn">Chat</Link></p>
+        </Col>
+        <Col xs={6} sm={4} md={3} className="trainerInfo">
+          <img
+            className="trainerImg"
+            src={trainer1}
+            alt="Trainer 1"
+          />
+          <h4 className="trainerName">Trainer's Name</h4>
+          <p className="trainerState">Trainer's Statement</p>
+          <p><Link to="/chat" className="chatBtn btn-primary btn-learn">Chat</Link></p>
+        </Col>
+        <Col xs={6} sm={4} md={3} className="trainerInfo">
+          <img
+            className="trainerImg"
+            src={trainer1}
+            alt="Trainer 1"
+          />
+          <h4 className="trainerName">Trainer's Name</h4>
+          <p className="trainerState">Trainer's Statement</p>
+          <p><Link to="/chat" className="chatBtn btn-primary btn-learn">Chat</Link></p>
+        </Col>
+      </Row>
     );
   }
 }
--- client/src/components/Filter.js
+++ client/src/components/Filter.js
@@ -1,11 +1,22 @@
 import React from "react";
+import {Row} from "react-bootstrap"
 
 class Filter extends React.Component {
 
   render() {
     return(
       <div>
-        Filter
+        <Row className="filterP">
+          <ul className="trainerFilter">
+            <li><a href="#">All</a></li>
+            <li><a href="#">Yoga</a></li>
+            <li><a href="#">Fitness</a></li>
+            <li><a href="#">HIIT</a></li>
+            <li><a href="#">Boxing</a></li>
+            <li><a href="#">Palates</a></li>
+            <li><a href="#">Taichi</a></li>
+          </ul>
+        </Row>
       </div>
     );
   }
