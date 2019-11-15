e5c5b90b1cb7986a3355d9a3c0a405f452dca0a2
sczhou06 <55016102+sczhou06@users.noreply.github.com>
2019-11-14 23:14:13 -0800

    Update About.js

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,11 +1,51 @@
 import React from "react";
+import bg1 from '../assets/img/bg1.jpg';
+import TopMenuBar from '../components/TopMenuBar'
 
-const About = () =>{
-    return (
-        <div>
-            About
-        </div>
-    );
+const bg = {
+    backgroundImage: `url(${bg1})`,
+    backgroundSize: "cover",
+    position:"fixed",
+    top: "0",
+    left: "0",
+    width:"100%",
+    height:"100%",
+    backgroundPosition: "center 0"
+}
+
+const text = {
+    width: "75%",
+    height: "100%",
+    margin: "300px auto",
+    color:"#acbfff",
+    fontFamily: "Gill Sans",
+    minWidth:"500px",
+    minHeight: "200px",
+    boxSizing: "border-box",
+    alignItems: "center",
+    fontSize: "2.5vw",
+    textAlign:"justify",
+    zIndex:"200"
 };
+
+class About extends React.Component {
+    constructor(props, context) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <div style={bg}>
+                    <TopMenuBar/>
+                    <p style={text}>
+                        GoFitness, provides one-on-one personal training online. Users could get a personalized training plan and instruction at a low cost, offering different solutions for different fitness groups.
+                    </p>
+                </div>
+            </div>
+        );
+    }
+}
+
 
 export default About;
